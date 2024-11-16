e1dbfd0b8dbc9b83545e7b9b16ffa186da3ebe09
skander Ben achour <benachour.skander1@gmail.com>
2024-11-16 11:51:12 +0100

    mcv component async

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,56 +1,57 @@
-import { NgModule, isDevMode } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import {
   provideHttpClient,
   withInterceptorsFromDi,
 } from "@angular/common/http";
+import { NgModule, isDevMode } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { ToastrModule } from "ngx-toastr";
 
 import { AppComponent } from "./app.component";
-import { FirstComponent } from "./components/first/first.component";
-import { SecondComponent } from "./components/second.component";
-import { ColorComponent } from "./components/color/color.component";
-import { TwoComponent } from "./components/two/two.component";
 import { CardProfilComponent } from "./components/card-profil/card-profil.component";
-import { PereComponent } from "./components/pere/pere.component";
+import { ColorComponent } from "./components/color/color.component";
 import { FilsComponent } from "./components/fils/fils.component";
+import { FirstComponent } from "./components/first/first.component";
+import { PereComponent } from "./components/pere/pere.component";
+import { SecondComponent } from "./components/second.component";
+import { TwoComponent } from "./components/two/two.component";
 
 import { AddCvComponent } from "./cv/add-cv/add-cv.component";
 import { CvComponent } from "./cv/cv/cv.component";
 import { DetailsCvComponent } from "./cv/details-cv/details-cv.component";
 
-import { NgstyleComponent } from "./directives/ngstyle/ngstyle.component";
 import { MiniWordComponent } from "./directives/mini-word/mini-word.component";
 import { NgclassComponent } from "./directives/ngclass/ngclass.component";
+import { NgstyleComponent } from "./directives/ngstyle/ngstyle.component";
 import { TodoComponent } from "./todo/todo/todo.component";
 
 import { HighlightDirective } from "./directives/highlight.directive";
 
-import { Btc2usdPipe } from "./pipes/btc2usd.pipe";
+import { AsyncPipe } from "@angular/common";
+import { ServiceWorkerModule } from "@angular/service-worker";
 import { AppRoutingModule } from "./app-routing.module";
+import { AuthInterceptorProvider } from "./auth/interceptors/auth.interceptor";
+import { LoginComponent } from "./auth/login/login.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
-import { FrontComponent } from "./templates/front/front.component";
-import { AdminComponent } from "./templates/admin/admin.component";
 import { NF404Component } from "./components/nf404/nf404.component";
 import { TestFormComponent } from "./components/test-form/test-form.component";
-import { LoginComponent } from "./auth/login/login.component";
-import { TestObservableComponent } from "./rxjs/test-observable/test-observable.component";
 import { TestHttpComponent } from "./components/test-http/test-http.component";
-import { AuthInterceptorProvider } from "./auth/interceptors/auth.interceptor";
-import { ListComponent } from "./cv/list/list.component";
-import { ItemComponent } from "./cv/item/item.component";
-import { DefaultImagePipe } from "./cv/pipes/default-image.pipe";
-import { EmbaucheComponent } from "./cv/embauche/embauche.component";
+import { AutocompleteComponent } from "./cv/autocomplete/autocomplete.component";
 import { CvCardComponent } from "./cv/cv-card/cv-card.component";
+import { EmbaucheComponent } from "./cv/embauche/embauche.component";
+import { ItemComponent } from "./cv/item/item.component";
+import { ListComponent } from "./cv/list/list.component";
+import { DefaultImagePipe } from "./cv/pipes/default-image.pipe";
 import { RhComponent } from "./optimizationPattern/rh/rh.component";
 import { UserListComponent } from "./optimizationPattern/user-list/user-list.component";
+import { Btc2usdPipe } from "./pipes/btc2usd.pipe";
 import { ProductsComponent } from "./products/products.component";
-import { ServiceWorkerModule } from "@angular/service-worker";
-import { AutocompleteComponent } from "./cv/autocomplete/autocomplete.component";
 import { SliderComponent } from "./rxjs/slider/slider.component";
+import { TestObservableComponent } from "./rxjs/test-observable/test-observable.component";
+import { AdminComponent } from "./templates/admin/admin.component";
+import { FrontComponent } from "./templates/front/front.component";
 
 @NgModule({
   declarations: [
@@ -94,6 +95,7 @@ import { SliderComponent } from "./rxjs/slider/slider.component";
   bootstrap: [AppComponent],
   imports: [
     BrowserModule,
+    AsyncPipe,
     FormsModule,
     BrowserAnimationsModule, // required animations module
     ToastrModule.forRoot(), // ToastrModule added
--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -1,37 +1,29 @@
 import { Component } from "@angular/core";
-import { Cv } from "../model/cv";
-import { LoggerService } from "../../services/logger.service";
 import { ToastrService } from "ngx-toastr";
+import { catchError, Observable, of } from "rxjs";
+import { LoggerService } from "../../services/logger.service";
+import { Cv } from "../model/cv";
 import { CvService } from "../services/cv.service";
 @Component({
   selector: "app-cv",
   templateUrl: "./cv.component.html",
-  styleUrls: ["./cv.component.css"],
+  styleUrls: ["./cv.component.css"]
 })
 export class CvComponent {
-  cvs: Cv[] = [];
-  selectedCv: Cv | null = null;
-  /*   selectedCv: Cv | null = null; */
+  cvs$: Observable<Cv[]>;
+  selectedCv$: Observable<Cv>;
   date = new Date();
 
   constructor(
     private logger: LoggerService,
     private toastr: ToastrService,
     private cvService: CvService
-  ) {
-    this.cvService.getCvs().subscribe({
-      next: (cvs) => {
-        this.cvs = cvs;
-      },
-      error: () => {
-        this.cvs = this.cvService.getFakeCvs();
-        this.toastr.error(`
-          Attention!! Les données sont fictives, problème avec le serveur.
-          Veuillez contacter l'admin.`);
-      },
-    });
-    this.logger.logger("je suis le cvComponent");
-    this.toastr.info("Bienvenu dans notre CvTech");
-    this.cvService.selectCv$.subscribe((cv) => (this.selectedCv = cv));
-  }
+  ) {this.cvs$ = this.cvService.getCvs().pipe(catchError(() => {
+    this.toastr.error(`
+              Attention!! Les données sont fictives, problème avec le serveur.
+              Veuillez contacter l'admin.`);
+    return of(this.cvService.getFakeCvs());
+  }));
+  this.selectedCv$ = this.cvService.selectCv$;
+}
 }
