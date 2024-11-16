87ffc65ee791061f477299d7a06836927d398218
skander Ben achour <benachour.skander1@gmail.com>
2024-11-16 06:07:38 +0100

    directive arc en ciel

---
+++ src/app/rainbow-input.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appRainbowInput]',
+  standalone: true
+})
+export class RainbowInputDirective {
+ private colors : string[] = ['red', 'blue', 'orange' , 'purple', 'yellow', 'pink']
+ @HostBinding('style.color') textColor:string= '';
+ @HostBinding('style.borderColor') borderColor: string ='';
+  constructor() { }
+  @HostListener('keyup') onKeyUp(){
+    const color=this.colors[Math.floor(Math.random()* this.colors.length)];
+    this.textColor = color
+    this.borderColor = color
+  }
+
+}
--- src/app/templates/front/front.component.ts
+++ src/app/templates/front/front.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RainbowInputDirective } from 'src/app/rainbow-input.directive';
 
 @Component({
     selector: 'app-front',
     templateUrl: './front.component.html',
     styleUrls: ['./front.component.css'],
     standalone: true,
-    imports: [RouterOutlet]
+    imports: [RouterOutlet, RainbowInputDirective]
 })
 export class FrontComponent {
 
